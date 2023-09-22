b55b091aee9f56baf22aae3586f3d64e7ad707d2
Alexm0u <alexm0u.13@gmail.com>
2023-09-22 23:50:36 +0200

    Creating Header Dropdown with all boards

---
+++ src/components/HeaderDropdown.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function HeaderDropdown() {
+  return (
+    <div className='py-10 px-6 absolute left-0 right-0 bottom-[-100vh} top-16 bg-[#00000080]'>
+
+    {/* DROPDOWN MODAL  */}
+
+        <div className="bg-white dark:bg-[#2b2c37] shadow-md shadow-[#364e7e1a] w-full py-4 rounded-xl">
+
+            <h3 className='dark:text-gray-300 text-gray-600 font-semibold mx-4 mb-8'>
+                All boards
+            </h3>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default HeaderDropdown
