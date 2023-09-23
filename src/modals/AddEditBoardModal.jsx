a894d5f8b148db876bd5f9ab7a0d828d6815d4d3
Alexm0u <alexm0u.13@gmail.com>
2023-09-23 12:49:02 +0200

    Added map & on change for update state on board columns

--- src/modals/AddEditBoardModal.jsx
+++ src/modals/AddEditBoardModal.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 function AddEditBoardModal({setBoardModalOpen , type, }) {
 
 const [name, setName] = useState('')
+const [newColumns, setNewColumns] = useState(
+  [
+    {name: 'Por hacer', task: [], id: uuidv4()},
+    {name: 'En curso', task: [], id: uuidv4()},
+    {name: 'Finalizada', task: [], id: uuidv4()}
+  ]
+)
+
+const onChange = (id, newValue) => {
+  setNewColumns((pervState) => {
+    const newState = [...pervState]
+    const column = newState.find((col)=> col.id === id)
+    column.name = newValue
+    return newState
+  })
+}
 
 
   return (
@@ -24,18 +41,39 @@ const [name, setName] = useState('')
 
        {/* TASK NAME */}
        <div className='mt-8 flex flex-col space-y-3'>
-        
+
+          <label className='text-sm dark:text-white text-grey-500'>
+            Board Columns
+          </label>
+
+          <input className='bg-transparent px-4 py-2 rounded-md text-sm border border-grey-600 outline-none
+          focus:outline-[#635fc7] outline-1 ring-0' placeholder='e.g Web Design'value={name} 
+          onChange={(e)=>{
+            setName(e.target.value);
+          }}
+          id='board-name-input'
+          />
+       </div>
+
+       {/* BOARD COLUMNS  */}
+
+       <div className='mt-8 flex flex-col space-y-3'>
         <label className='text-sm dark:text-white text-grey-500'>
           Board Columns
         </label>
-
-        <input className='bg-transparent px-4 py-2 rounded-md text-sm border border-grey-600 focus:outline-[#635fc7] 
-        outline-1 ring-0' placeholder='e.g Web Design'value={name} onChange={(e)=>{
-          setName(e.target.value);
-        }}
-        id='board-name-input'
-        />
-
+        {newColumns.map((column,index) => (
+          <div key={index} className='flex items-center w-full'>
+            <input className='bg-transparent flex-grow px-4 py-2 
+            rounded-md text-sm border border-grey-600 outline-none focus:outline-[#735fc7]'
+            onChange={(e)=>{
+              onChange(column.id, e.target.value)
+            }}
+            value={column.name}
+            type='text' 
+            />
+          </div>
+        ))
+        }
        </div>
       </div>
 
