a831845938e1db08e288e5ae92fc3f01d15e8ee5
Alexm0u <alexm0u.13@gmail.com>
2023-09-23 20:22:43 +0200

    FIX: Edit board and create board fixed!!!!!!!!!!!!!!

--- src/modals/AddEditBoardModal.jsx
+++ src/modals/AddEditBoardModal.jsx
@@ -1,160 +1,163 @@
-import React, { useState } from 'react'
-import {v4 as uuidv4} from 'uuid'
-import crossIcon from "../assets/icon-cross.svg"
-import { useDispatch, useSelector } from 'react-redux'
-import boardSlices from '../redux/boardsSlice'
-import boardsSlice from '../redux/boardsSlice'
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import crossIcon from "../assets/icon-cross.svg";
+import { useDispatch, useSelector } from 'react-redux';
+import boardSlices from '../redux/boardsSlice';
 
-function AddEditBoardModal({setBoardModalOpen , type, }) {
+function AddEditBoardModal({ setBoardModalOpen, type }) {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [newColumns, setNewColumns] = useState([
+    { name: 'Por hacer', task: [], id: uuidv4() },
+    { name: 'En curso', task: [], id: uuidv4() },
+    { name: 'Finalizada', task: [], id: uuidv4() }
+  ]);
 
-const dispatch = useDispatch()
-const [name, setName] = useState('')
-const [isFirstLoad, setisFirstLoad] = useState(true)
-const [isValid, setIsValid] = useState
-const board = useSelector((state) => state.boards).find(
-  (board)=> board.isActive )
-const [newColumns, setNewColumns] = useState(
-  [
-    {name: 'Por hacer', task: [], id: uuidv4()},
-    {name: 'En curso', task: [], id: uuidv4()},
-    {name: 'Finalizada', task: [], id: uuidv4()}
-  ]
-)
-if (type === 'edit' && isFirstLoad) {
-    setNewColumns(
-      board.columns.map((col)=> {
-        return {...col,id:uuidv4()}
-      })
-    )
-    setName(board.name)
-    setisFirstLoad(false)
-}
+  const board = useSelector((state) => state.boards).find(
+    (board) => board.isActive
+  );
 
-
-const onChange = (id, newValue) => {
-  setNewColumns((pervState) => {
-    const newState = [...pervState]
-    const column = newState.find((col)=> col.id === id)
-    column.name = newValue
-    return newState
-  })
-}
-
-const onDelete = (id) => {
-  setNewColumns((perState)=> perState.filter((el)=> el.id !== id))
-}
-
-const validate = () => {
-  setIsValid(false)
-  if (!name.trim()) {
-    return false
-  }
-
-  for (let i = 0; i < newColumns.length; i++) {
-    if (!newColumns[i].name.trim()){
-      return false
+  useEffect(() => {
+    // Si el tipo es "edit", establece los datos iniciales del tablero
+    if (type === 'edit' && board) {
+      setName(board.name);
+      setNewColumns(board.columns.map((col) => ({ ...col, id: uuidv4() })));
     }
-  }
-  setIsValid(true)
-  return true
-}
+  }, [type, board]);
 
-const onSubmit = (type) => {
-  setBoardModalOpen(false)
-  if (type === 'add') {
-    dispatch(boardSlices.actions.addBoard({name, newColumns}))
-  }else{
-    dispatch(boardsSlice.actions.editBoard({name, newColumns}))
-  }
-}
+  const onChange = (id, newValue) => {
+    setNewColumns((prevState) => {
+      const newState = [...prevState];
+      const column = newState.find((col) => col.id === id);
+      column.name = newValue;
+      return newState;
+    });
+  };
 
+  const onDelete = (id) => {
+    setNewColumns((prevState) =>
+      prevState.filter((el) => el.id !== id)
+    );
+  };
+
+  const validate = () => {
+    setIsValid(false);
+    if (!name.trim()) {
+      return false;
+    }
+
+    for (let i = 0; i < newColumns.length; i++) {
+      if (!newColumns[i].name.trim()) {
+        return false;
+      }
+    }
+    setIsValid(true);
+    return true;
+  };
+
+  const onSubmit = () => {
+    if (type === 'add') {
+      dispatch(boardSlices.actions.addBoard({ name, newColumns }));
+    } else if (type === 'edit' && board) {
+      // Asegúrate de que los datos se estén pasando correctamente
+      const editedBoard = {
+        id: board.id,
+        name,
+        columns: newColumns,
+      };
+      dispatch(boardSlices.actions.editBoard(editedBoard));
+    }
+
+    // Cerrar el modal después de agregar o editar el tablero
+    setBoardModalOpen(false);
+  };
 
   return (
-    <div onClick={(e)=> {
-      if (e.target !== e.currentTarget){
-        return
-      }
-      setBoardModalOpen(false)
-    }}
-    className='fixed right-0 left-0 top-0 bottom-0 px-2 scrollbar-hide py-4 overflow-scroll
-    z-50 justify-center items-center flex bg-[#00000080}'>
-
+    <div
+      onClick={(e) => {
+        if (e.target !== e.currentTarget) {
+          return;
+        }
+        setBoardModalOpen(false);
+      }}
+      className="fixed right-0 left-0 top-0 bottom-0 px-2 scrollbar-hide py-4 overflow-scroll z-50 justify-center items-center flex bg-[#00000080]"
+    >
       {/* MODAL SECTION */}
-      <div className='scrollbar-hide overflow-y-scroll max-h-[95vh} bg-white dark:bg-[#2b2c37] 
-      text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl'>
-        <h3 className='text-lg'>
-          {type === 'edit'? 'Edit': 'Add New'} Board
+      <div className="scrollbar-hide overflow-y-scroll max-h-[95vh] bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl">
+        <h3 className="text-lg">
+          {type === 'edit' ? 'Edit' : 'Add New'} Board
         </h3>
 
-       {/* NOMBRE TAREA */}
-       <div className='mt-8 flex flex-col space-y-3'>
-
-          <label className='text-sm dark:text-white text-grey-500'>
+        {/* NOMBRE TAREA */}
+        <div className="mt-8 flex flex-col space-y-3">
+          <label className="text-sm dark:text-white text-grey-500">
             Board Columns
           </label>
 
-          <input className='bg-transparent px-4 py-2 rounded-md text-sm border border-grey-600 outline-none
-          focus:outline-[#635fc7] outline-1 ring-0' placeholder='e.g Web Design'value={name} 
-          onChange={(e)=>{
-            setName(e.target.value);
-          }}
-          id='board-name-input'
+          <input
+            className="bg-transparent px-4 py-2 rounded-md text-sm border border-grey-600 outline-none focus:outline-[#635fc7] outline-1 ring-0"
+            placeholder="e.g Web Design"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            id="board-name-input"
           />
-       </div>
+        </div>
 
-       {/* BOARD COLUMNS  */}
+        {/* BOARD COLUMNS  */}
+        <div className="mt-8 flex flex-col space-y-3">
+          <label className="text-sm dark:text-white text-grey-500">
+            Board Columns
+          </label>
+          {newColumns.map((column, index) => (
+            <div key={column.id} className="flex items-center w-full">
+              <input
+                className="bg-transparent flex-grow px-4 py-2 rounded-md text-sm border border-grey-600 outline-none focus:outline-[#735fc7]"
+                onChange={(e) => {
+                  onChange(column.id, e.target.value);
+                }}
+                value={column.name}
+                type="text"
+              />
+              <img
+                src={crossIcon}
+                className="cursor-pointer m-4"
+                onClick={() => {
+                  onDelete(column.id);
+                }}
+              />
+            </div>
+          ))}
+        </div>
 
-       <div className='mt-8 flex flex-col space-y-3'>
-        <label className='text-sm dark:text-white text-grey-500'>
-          Board Columns
-        </label>
-        {newColumns.map((column,index) => (
-          <div key={index} className='flex items-center w-full'>
-            <input className='bg-transparent flex-grow px-4 py-2 
-            rounded-md text-sm border border-grey-600 outline-none focus:outline-[#735fc7]'
-            onChange={(e)=>{
-              onChange(column.id, e.target.value)
-            }}
-            value={column.name}
-            type='text' 
-            />
-            <img src={crossIcon}
-            className='cursor-pointer m-4'
+        <div>
+          <button
+            className="w-full items-center hover:opacity-75 dark:text-[#635fc7] dark:bg-white text-white bg-[#635fc7] mt-2 py-2 rounded-full"
             onClick={() => {
-              onDelete(column.id)
+              setNewColumns((state) => [
+                ...state,
+                { name: '', task: [], id: uuidv4() },
+              ]);
             }}
-            />
-          </div>
-        ))
-        }
-       </div>
+          >
+            Añadir columna
+          </button>
 
-       <div>
-        <button className='w-full items-center hover:opacity-75
-         dark:text-[#635fc7] dark:bg-white text-white bg-[#635fc7] mt-2 py-2 rounded-full'
-         onClick={()=> {
-          setNewColumns((state)=>[...state,{name: '', task: [], id: uuidv4()}])
-         }}
-         >
-          Añadir columna
-        </button>
-
-        <button className='w-full items-center hover:opacity-75
-         dark:text-white dark:bg-[#635fc7] mt-8 relative text-white bg-[#635fc7] py-2 rounded-full'
-         onClick={
-          () => {
-            const isValid = validate()
-            if (isValid === true) onSubmit(type)
-          }
-         }
-         >
-          {type === 'add'? 'Crear nuevo tablero': 'Guardar cambios'}
-        </button>
-       </div>
+          <button
+            className="w-full items-center hover:opacity-75 dark:text-white dark:bg-[#635fc7] mt-8 relative text-white bg-[#635fc7] py-2 rounded-full"
+            onClick={() => {
+              const isValid = validate();
+              if (isValid === true) onSubmit();
+            }}
+          >
+            {type === 'add' ? 'Crear nuevo tablero' : 'Guardar cambios'}
+          </button>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default AddEditBoardModal
+export default AddEditBoardModal;
