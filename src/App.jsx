a91b3a62ee099c01a574c199ed0f755023835d1e
Alexm0u <alexm0u.13@gmail.com>
2023-09-23 21:55:09 +0200

    Fixed import error about empty board on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
 import Center from './components/Center'
+import EmptyBoard from './components/EmptyBoard'
+import boardsSlice from './redux/boardsSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch()
+
+  const boards = useSelector ((state)  => state.boards)
+  const activeBoard = boards.find(board => board.isActive)
+  if (!activeBoard && boards.length > 0) {
+    dispatch(boardsSlice.actions.setBoardActive({index:0}))}
+  
 
   const [boardModalOpen, setBoardModalOpen] = useState(false)
 
   return (
-    <div>
-
-    {/* HEADER SECTION */}
+    <div className='overflow-hidden overflow-x-scroll'>
+      <>
+      {
+      boards.length > 0 ? 
+      <>
+      {/* HEADER SECTION */}
       <Header boardModalOpen = {boardModalOpen} setBoardModalOpen = {setBoardModalOpen}/>
-
-
       {/* CENTER SECTION */}
+      </>
+      :
+      <>
+      <EmptyBoard type='add'/>
+      </>
+      }
       <Center/>
+      </>
     </div>
   )
 }
