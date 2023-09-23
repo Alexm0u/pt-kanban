2475a4b14d3ed33de25ff7dda851a5d8035c3755
Alexm0u <alexm0u.13@gmail.com>
2023-09-23 12:04:55 +0200

    Adding structure to Board Columns and adding scrollbar hide from tailwind

--- src/modals/AddEditBoardModal.jsx
+++ src/modals/AddEditBoardModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function AddEditBoardModal({setBoardModalOpen}) {
+function AddEditBoardModal({setBoardModalOpen , type, }) {
   return (
     <div onClick={(e)=> {
       if (e.target !== e.currentTarget){
@@ -8,8 +8,23 @@ function AddEditBoardModal({setBoardModalOpen}) {
       }
       setBoardModalOpen(false)
     }}
-    className='fixed right-0 left-0 top-0 bottom-0 px-2 py-4 overflow-scroll
+    className='fixed right-0 left-0 top-0 bottom-0 px-2 scrollbar-hide py-4 overflow-scroll
     z-50 justify-center items-center flex bg-[#00000080}'>
+
+      {/* MODAL SECTION */}
+      <div className='scrollbar-hide overflow-y-scroll max-h-[95vh} bg-white dark:bg-[#2b2c37] 
+      text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl'>
+        <h3 className='text-lg'>
+          {type === 'edit'? 'Edit': 'Add New'} Board
+        </h3>
+
+       {/* TASK NAME */}
+       <div className='mt-8 flex flex-col space-y-3'>
+        <label className='text-sm dark:text-white text-grey-500'>
+          Board Columns
+        </label>
+       </div>
+      </div>
 
     </div>
   )
